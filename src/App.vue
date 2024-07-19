4e80d822278e95309d2974fce95eb8ebaba85acf
Boryana Kircheva (HAU-EDCP) <boryana.kircheva@liebherr.com>
2024-07-19 14:57:24 +0300

    test2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to my portfolio" />
+  <HelloWorld msg="Welcome to my portfolio!!" />
 </template>
 
 <script>
