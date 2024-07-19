63a4b91a5d5a5ef76116108e94be06a552633329
Boryana Kircheva (HAU-EDCP) <boryana.kircheva@liebherr.com>
2024-07-19 17:51:33 +0300

    return

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === "production"
-      ? "/" // Replace 'repository-name' with your GitHub repository name
+      ? "/portfolio/" // Replace 'repository-name' with your GitHub repository name
       : "/",
   transpileDependencies: ["vue-clamp", "resize-detector"], // Add any additional dependencies to transpile if needed
 };
